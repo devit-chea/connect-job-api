42c9ee74cb5cb53e9dba6851de9cf2cc04041238
davitchea <144406950+davitchea@users.noreply.github.com>
2026-06-14 20:04:26 +0700

    update entrypoint server

--- entrypoint-server.sh
+++ entrypoint-server.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Set default values if environment variables are not set
-GUNICORN_WORKERS="${GUNICORN_WORKERS:-2}"
-GUNICORN_THREADS="${GUNICORN_THREADS:-2}"
+GUNICORN_WORKERS="${GUNICORN_WORKERS:-$(( 2 * $(nproc) + 1 ))}"
+GUNICORN_THREADS="${GUNICORN_THREADS:-4}"
 
 # Apply database migrations
 echo "Applying database migrations..."
@@ -27,6 +27,6 @@ python manage.py insert_mail_template
 
 exec gunicorn config.wsgi \
     --bind 0.0.0.0:8000 \
-    --timeout 30 \
+    --timeout 120 \
     --workers "$GUNICORN_WORKERS" \
     --threads "$GUNICORN_THREADS"
